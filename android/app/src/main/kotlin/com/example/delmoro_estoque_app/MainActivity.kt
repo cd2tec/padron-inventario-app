5d7e9db20a91c99bfbf571fb29b8e2f15e5d2fa4
Fernando Cardoso <54037103+fernandoc89@users.noreply.github.com>
2024-01-30 10:44:44 -0300

    create app, login screen and authenticate

---
+++ android/app/src/main/kotlin/com/example/delmoro_estoque_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.delmoro_estoque_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/popup-button.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class PopupMenuButtonExample1 extends StatefulWidget {
+  const PopupMenuButtonExample1({Key? key}) : super(key: key);
+
+  @override
+  _PopupMenuButtonExample1State createState() =>
+      _PopupMenuButtonExample1State();
+}
+
+class _PopupMenuButtonExample1State extends State {
+  var selectedItem = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("AppMaking.com"),
+        centerTitle: true,
+        actions: [
+          PopupMenuButton(onSelected: (value) {
+            // your logic
+            setState(() {
+              selectedItem = value.toString();
+            });
+            print(value);
+            Navigator.pushNamed(context, value.toString());
+          }, itemBuilder: (BuildContext bc) {
+            return const [
+              PopupMenuItem(
+                child: Text("Consultar Estoque"),
+                value: '/estoque',
+              ),
+              PopupMenuItem(
+                child: Text("Gerenciar Perfis"),
+                value: '/perfis',
+              ),
+            ];
+          })
+        ],
+      ),
+      body: Center(
+        child: Text(selectedItem),
+      ),
+    );
+  }
+}
